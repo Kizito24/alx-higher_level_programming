1ec814ce192e9d17b2c965c6ea2e83386dd3b089
Chinoyelum Kizito Chiazor <kizitochiazor@gmail.com>
2024-04-16 08:54:33 +0000

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  const occur = list.filter((element) => element === searchElement);
-  return occur.length;
-};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const logs = [0];
-exports.logMe = function (item) {
-  console.log(`${logs[logs.length - 1]}: ${item}`);
-  logs.push(logs[logs.length - 1] + 1);
-};
